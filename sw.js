eb68f78b8f92918e04bc6c5e5b2d7cbfe5c5e403
Neto Noronha <netto99@gmail.com>
2026-09-25 19:32:29 -0300

    Atualizar cache da nova moldura

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='beth-story-v2';
+const CACHE='beth-story-v5';
 const ASSETS=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(
