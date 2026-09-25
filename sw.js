591b17536c89bbf62c49c6a46f00779575f722be
Neto Noronha <netto99@gmail.com>
2026-09-25 18:55:33 -0300

    Transformar gerador em Story 9x16 Beth Noronha

--- sw.js
+++ sw.js
@@ -1,5 +1,7 @@
-const CACHE='molduras-v1';
+const CACHE='beth-story-v2';
 const ASSETS=['/','/index.html','/styles.css','/app.js','/manifest.webmanifest','/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('activate',e=>e.waitUntil(
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())
+));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
